e31d6d09051d912c6cfc7fadf8745f8741fcfe47
liuwei <liuweiccy@qq.com>
2017-07-11 11:22:43 +0800

    lambda表达式简化编程

--- kotlintest/src/main/kotlin/com/digisky/Collection.kt
+++ kotlintest/src/main/kotlin/com/digisky/Collection.kt
@@ -1,8 +1,18 @@
 package com.digisky
 
-fun main(args: Array<String>) {
+fun main1(args: Array<String>) {
     val items = listOf("apple", "banana", "kiwi")
     for (item in items) {
         println(item)
     }
+}
+
+fun main(args: Array<String>) {
+    val items = setOf("apple", "banana", "kiwi")
+
+    items
+            .filter { it.startsWith("a") }
+            .sortedBy { it }
+            .map { it.toUpperCase() }
+            .forEach { println(it) }
 }
