50966fec5d236a314062ba801ea79d9295a2b46c
liuwei <liuweiccy@qq.com>
2017-07-11 10:51:05 +0800

    删除无用的代码

--- kotlintest/src/main/kotlin/com/digisky/Hello.kt
+++ kotlintest/src/main/kotlin/com/digisky/Hello.kt
@@ -1,5 +1,11 @@
 package com.digisky
 
+data class Person(val name : String, var age : Int)
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+    val p = Person("BigVV", 26)
+    val (name, age) = p
+    println("$name -> $age")
+    val p1 = p.copy(name = "BigLiu", age = 30)
+    println("${p1.name} -> ${p1.age}")
 }
