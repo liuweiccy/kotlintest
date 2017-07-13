2b8c01594500ffdda1403aa8abf3cae6165f0077
liuwei <liuweiccy@qq.com>
2017-07-13 17:44:30 +0800

    添加忽略文件

--- kotlintest/src/main/kotlin/com/digisky/Collection.kt
+++ kotlintest/src/main/kotlin/com/digisky/Collection.kt
@@ -1,5 +1,7 @@
 package com.digisky
 
+import redis.clients.jedis.Jedis
+
 fun main1(args: Array<String>) {
     val items = listOf("apple", "banana", "kiwi")
     for (item in items) {
@@ -10,9 +12,13 @@ fun main1(args: Array<String>) {
 fun main(args: Array<String>) {
     val items = setOf("apple", "banana", "kiwi")
 
-    items
-            .filter { it.startsWith("a") }
-            .sortedBy { it }
-            .map { it.toUpperCase() }
-            .forEach { println(it) }
+    items.filter { it.startsWith("a") }
+         .sortedBy { it }
+         .map { it.toUpperCase() }
+         .forEach { println(it) }
+    val jedis = Jedis("192.168.2.214", 6333)
+    jedis.auth("123456")
+    jedis.set("kotlin", "hello")
+    println("get redis value ${jedis.get("kotlin")}")
+    jedis.del("kotlin")
 }
